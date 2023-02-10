fa107b4d885635044838982081b05575b4697f34
Erika <55201104+ErikaZimmer@users.noreply.github.com>
2023-02-10 18:35:58 -0300

    Add files via upload

---
+++ javascript/02. Control de flujos/funciones.js
@@ -0,0 +1,14 @@
+var nombreDelUsuario = prompt("Hola extraño, danos tu nombre");
+var numeroDelUsuario = parseInt(prompt("Hola extraño, danos un numero"));
+
+if (numeroDelUsuario<31){
+alert(nombreDelUsuario + " el numero que ingresaste es menor a 31");
+}
+else if (numeroDelUsuario===31){
+alert(nombreDelUsuario + " sin querer adivinaste mi edad XD");
+}
+else{
+alert(nombreDelUsuario + " el numero que ingresaste es mayor a 31");
+}
+
+document.write("Hola mundo");
