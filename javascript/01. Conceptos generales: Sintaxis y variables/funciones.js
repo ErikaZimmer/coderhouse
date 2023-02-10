d583380d5852c554bddcf704c894eb223b5c9b31
Erika <55201104+ErikaZimmer@users.noreply.github.com>
2023-02-10 18:33:07 -0300

    Add files via upload

---
+++ javascript/01. Conceptos generales: Sintaxis y variables/funciones.js
@@ -0,0 +1,6 @@
+var nombreDelUsuario = prompt("Hola extraño, danos tu nombre");
+var numeroDelUsuario = prompt("Hola extraño, danos un numero");
+var resultado= numeroDelUsuario*2;
+alert(nombreDelUsuario + " el doble de " + numeroDelUsuario + " es " + resultado);
+
+document.write("Hola mundo");
